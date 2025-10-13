a581f4c97bc45d83be0b330277e9f1eb64b7cb0d
Cherry <aazetthr@gmail.com>
2025-10-13 22:06:08 +0300

    fix: msvc part 2

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
-const nfd = require(__dirname + "nfd_bindings.node");
+let nfd = require("./dist/nfd_bindings.node");
+if (!nfd) {
+  nfd = require("./dist/Release/nfd_bindings.node");
+}
 
 export function openDialog(filters: Record<string, string>): string {
   return nfd.openDialog(filters);
