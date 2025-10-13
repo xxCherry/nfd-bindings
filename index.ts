58a044ec0a7e11125ec6c5f5be112a34c275a19d
Cherry <aazetthr@gmail.com>
2025-10-13 22:01:30 +0300

    fix: module path

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-const nfd = require("./dist/module.node");
+const nfd = require("./dist/nfd_bindings.node");
 
 export function openDialog(filters: Record<string, string>): string {
   return nfd.openDialog(filters);
